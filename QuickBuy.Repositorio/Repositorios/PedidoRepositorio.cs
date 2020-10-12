1c2c4ec2a54326304a295a28172e62737ff33747
wilsesa <wilson.sanchez14@hotmail.com>
2020-10-12 17:35:41 -0300

    Criado os Repositorios

---
+++ QuickBuy.Repositorio/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class PedidoRepositorio : BaseRepositorio<Pedido>, IPedidoRepositorio
+    {
+    }
+}
