fd65da67ca20ce2ed706fab4a5b04a79d0a2b17f
wilsesa <wilson.sanchez14@hotmail.com>
2020-10-12 14:21:52 -0300

    Criação das entidade

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        /// <summary>
+        /// Um usuario pode ter nenhum ou muitos pedidos
+        /// </summary>
+
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
