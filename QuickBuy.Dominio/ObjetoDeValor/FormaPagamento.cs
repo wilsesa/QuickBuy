b7e720626bfe5b797a0972bcde5f8d4b28968511
wilsesa <wilson.sanchez14@hotmail.com>
2020-10-13 22:22:45 -0300

    Criacao as entidades validas

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,35 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+
+        public bool Boleto
+        {
+            get { return Id  == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+
+        public bool NaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
