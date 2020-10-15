a832db95c4cb6f5e88c42b0034e1da9f81a03b8c
wilsesa <wilson.sanchez14@hotmail.com>
2020-10-14 21:44:43 -0300

    Realizado de mapeamento entre relacionamento de entidades de propiedades

---
+++ QuickBuy.Repositorio/Config/ItemPedidoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class ItemPedidoConfiguration : IEntityTypeConfiguration<ItemPedido>
+    {
+        public void Configure(EntityTypeBuilder<ItemPedido> builder)
+        {
+            builder.HasKey(i => i.Id);
+            //Builder utiliza o padrão Fluent
+            builder.Property(i => i.ProdutoId).IsRequired();
+            builder.Property(i => i.Quantidade).IsRequired();
+        }
+    }
+}
